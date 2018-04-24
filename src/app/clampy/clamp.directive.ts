0d377ee5be0a394d42b9a64acb4af88c8e538530
Alexandre Moore <alexandre.moore@gmail.com>
2018-04-24 18:12:34 -0400

    Update ng-packagr, now use element-resize-detector and emits only when clamped

--- src/app/clampy/clamp.directive.ts
+++ src/app/clampy/clamp.directive.ts
@@ -1,17 +1,30 @@
-import { AfterViewInit, Directive, ElementRef, Input, Renderer2, SecurityContext, Output, EventEmitter } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import * as clampy_ from '@clampy-js/clampy/dist/clampy.umd.js';
-import isNil from 'lodash-es/isNil';
+import {
+  AfterViewInit,
+  Directive,
+  ElementRef,
+  Input,
+  Renderer2,
+  SecurityContext,
+  Output,
+  EventEmitter,
+  OnDestroy
+} from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
+import * as clampy_ from "@clampy-js/clampy/dist/clampy.umd.js";
+import * as elementResizeDetectorMaker_ from "element-resize-detector";
+import isEmpty from "lodash-es/isEmpty";
+import isNil from "lodash-es/isNil";
 
 // https://github.com/rollup/rollup/issues/670#issuecomment-284621537
 const clampy: any = (<any>clampy_).default || clampy_;
+const elementResizeDetectorMaker: any =
+  (<any>elementResizeDetectorMaker_).default || elementResizeDetectorMaker_;
 
 @Directive({
-  selector: '[clampy]'
+  selector: "[clampy]"
 })
-export class ClampDirective implements AfterViewInit {
-
-  /**
+export class ClampDirective implements AfterViewInit, OnDestroy {
+  /*
    * This controls where and when to clamp the text of an element.
    * Submitting a number controls the number of lines that should be displayed.
    * Second, you can submit a CSS value (in px or em) that controls the height
@@ -19,77 +32,110 @@ export class ClampDirective implements AfterViewInit {
    * string. Auto will try to fill up the available space with the content and
    * then automatically clamp once content no longer fits.
    *
-   * @type {string}
+   * @type string
    * @memberof ClampDirective
    */
   @Input() public clampy: string;
 
-  /**
+  /*
      * Sometimes you need to apply an ellipsis on HTML content. The prefered Angular way to usually do this is to bind the
      * HTML content to the innerHTML attribute. However, this directive also modifies the innerHTML property and this may
      * produce unexpected results.
      *
-     * To counter this, you can instead bind it to the htgEllipsisContent attribute.
+     * To counter this, you can instead bind it to the clampyContent attribute.
      * The content will be automatically sanitized by the directive so that only safe HTML content will be present.
      *
-     * @type {string}
+     * @type string
      * @memberof ClampDirective
      */
   @Input() public clampyContent: string;
 
-  /**
+  /*
    * The character to insert at the end of the HTML element after truncation is
    * performed. This defaults to an ellipsis (…).
    *
-   * @type {string}
+   * @type string
    * @memberof ClampDirective
    */
   @Input() public clampyTruncationCharacter: string;
 
-  /**
+  /*
    * The original content before any ellipsis applied.
    *
-   * @type {EventEmitter<string>}
+   * @type EventEmitter<string>
    * @memberof ClampDirective
    */
-  @Output() public originalContent: EventEmitter<string> = new EventEmitter<string>();
+  @Output()
+  public originalContent: EventEmitter<string> = new EventEmitter<string>();
 
   private initialContent: string;
+  private resizeDetector = elementResizeDetectorMaker({ strategy: "scroll" });
+  private element: HTMLElement;
 
   constructor(
     private elementRef: ElementRef,
     private renderer: Renderer2,
     private sanitizer: DomSanitizer
-  ) { }
+  ) {
+    this.element = this.elementRef.nativeElement;
+  }
 
   ngAfterViewInit(): void {
-    const element: HTMLElement = this.elementRef.nativeElement;
-    if (isNil(this.clampyContent)) {
-      this.initialContent = element.innerHTML;
-    } else {
-      this.initialContent = this.sanitizer.sanitize(SecurityContext.HTML, this.clampyContent);
+    // We use element-resize-detector to trigger the ellipsis.
+    this.resizeDetector.listenTo(this.element, () => {
+      this.doClampElement(this.element);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.resizeDetector.removeAllListeners(this.element);
+  }
+
+  private setInitialContent(): void {
+    if (isNil(this.initialContent)) {
+      if (isNil(this.clampyContent)) {
+        this.initialContent = isEmpty(this.element.innerHTML)
+          ? undefined
+          : this.element.innerHTML;
+      } else {
+        this.initialContent = this.sanitizer.sanitize(
+          SecurityContext.HTML,
+          this.clampyContent
+        );
+      }
+    }
+  }
+
+  private doClampElement(element: HTMLElement): void {
+    // We use element-resize-detector to trigger the ellipsis.
+    // Element-resize-detector adds an inner div to monitor
+    // it's scroll events.
+    // The process of truncating the text for ellipsis removes this div, so we need to remove and readd it
+    const scrollNode: Element = this.element.querySelector(
+      ".erd_scroll_detection_container"
+    );
+    if (scrollNode) {
+      this.element.removeChild(scrollNode);
     }
 
-    this.originalContent.emit(this.initialContent);
-
-    element.innerHTML = this.initialContent;
-
-    // Set the opactity to 0 to avoid content to flick when clamping.
-    this.renderer.setStyle(element, 'opacity', 0);
+    this.element = this.elementRef.nativeElement;
+    this.setInitialContent();
 
     // Set back the element content to the original before clamping.
     // This is necessary when the container grows back.
-    element.innerHTML = this.initialContent;
+    if (!isNil(this.initialContent)) {
+      this.element.innerHTML = this.initialContent;
+      this.clampElement(this.element);
+    }
 
-    this.clampElement(element);
-
-    // Set the opacity back to 1 now that the content is clamped.
-    this.renderer.setStyle(element, 'opacity', 1);
+    if (scrollNode) {
+      this.element.appendChild(scrollNode);
+    }
   }
 
   private clampElement(element: HTMLElement): void {
-    let clampSize = 'auto';  // Default clamp size based on available height
-    let truncationChar = '…';
+    let clampSize = "auto"; // Default clamp size based on available height
+    let truncationChar = "…";
 
     if (this.clampy) {
       clampSize = this.clampy;
@@ -102,12 +148,23 @@ export class ClampDirective implements AfterViewInit {
     const options = {
       clamp: clampSize,
       truncationChar: truncationChar,
-      // Clamp.js will try to use native clamp if available in the browser
+      // Clampy will try to use native clamp if available in the browser
       // however this can leads to unexpected results so we need to explicitely
       // disable it.
       useNativeClamp: false
     };
 
-    clampy.clamp(element, options);
+    // Set the opactity to 0 to avoid content to flick when clamping.
+    this.renderer.setStyle(element, "opacity", 0);
+    const result = clampy.clamp(element, options);
+
+    // Set the opacity back to 1 now that the content is clamped.
+    this.renderer.setStyle(element, "opacity", 1);
+
+    if (result.original !== result.clamped) {
+      this.originalContent.emit(result.original);
+      // console.log('result.original', result.original);
+      console.log('result.clamped', result.clamped);
+    }
   }
 }
